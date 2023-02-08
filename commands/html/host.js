984519a94555b0ebafba76e012b3ed6dbae97a3f
Eric Bellamy <ericbellamy@rogers.com>
2023-02-08 18:00:57 -0500

    integrate ngrok with HTTPS

--- commands/html/host.js
+++ commands/html/host.js
@@ -1,8 +1,14 @@
 const fs = require('fs-extra');
+const ngrok = require('ngrok');
+
 const build = require('./build.js');
 
 let startHost = true;
-let devExit = true;
+let devExit = false;
+tired.HOST_CONFIG = {
+	server: { status: true },
+	ngrok: { status: true },
+}
 module.exports = async function (PORT_NUMBER, EMPTY = true) {
 	if (EMPTY) fs.emptyDirSync('.tired/html');
 	fs.ensureDirSync('.tired/html/dist');
@@ -15,11 +21,29 @@ module.exports = async function (PORT_NUMBER, EMPTY = true) {
 
 		console.log();
 
-		if (devExit) return process.exit();
+		// If we don't need a server
+		if (tired.HOST_CONFIG.server.status === false) return process.exit();
 
 		// Host HTTP light-server (Hot reload)
 		if (startHost) {
 			startHost = false;
+
+			if (tired.HOST_CONFIG.ngrok.status) {
+				const ngrokConfig = {
+					addr: `https://0.0.0.0:${PORT_NUMBER}`,
+					region: 'us'
+				}
+				if(tired.HOST_CONFIG.ngrok.subdomain != undefined && tired.HOST_CONFIG.ngrok.authtoken != undefined){
+					ngrokConfig.subdomain = tired.HOST_CONFIG.ngrok.subdomain;
+					ngrokConfig.authtoken = tired.HOST_CONFIG.ngrok.authtoken;
+				}
+				tired.HOST_CONFIG.ngrok.url = await ngrok.connect(ngrokConfig);
+
+				tired.log.color.start('host.js/ngrok');
+				tired.log.color('host.js', ['cyan', tired.HOST_CONFIG.ngrok.url + '\n']);
+				tired.log.color.stop('host.js/ngrok');
+			}
+
 			tired.root.require('lib/public/html/host/lightserver.js')(PORT_NUMBER);
 		}
 	});
