b2701f5d047ffe0b4aa902e5748f10678bff0520
Eric Bellamy <ericbellamy@rogers.com>
2023-02-08 18:09:26 -0500

    better server url logging

--- commands/html/host.js
+++ commands/html/host.js
@@ -7,7 +7,7 @@ let startHost = true;
 let devExit = false;
 tired.HOST_CONFIG = {
 	server: { status: true },
-	ngrok: { status: true },
+	ngrok: { status: false },
 }
 module.exports = async function (PORT_NUMBER, EMPTY = true) {
 	if (EMPTY) fs.emptyDirSync('.tired/html');
@@ -38,10 +38,6 @@ module.exports = async function (PORT_NUMBER, EMPTY = true) {
 					ngrokConfig.authtoken = tired.HOST_CONFIG.ngrok.authtoken;
 				}
 				tired.HOST_CONFIG.ngrok.url = await ngrok.connect(ngrokConfig);
-
-				tired.log.color.start('host.js/ngrok');
-				tired.log.color('host.js', ['cyan', tired.HOST_CONFIG.ngrok.url + '\n']);
-				tired.log.color.stop('host.js/ngrok');
 			}
 
 			tired.root.require('lib/public/html/host/lightserver.js')(PORT_NUMBER);
